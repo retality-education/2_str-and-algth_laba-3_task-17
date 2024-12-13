fd806751bdd20f51b4789e6e2060bae86f216fb5
Vitalii Kertanov <RetalityAWD@yandex.ru>
2024-12-13 16:52:55 +0300

    solution now is optimized and right

--- laba-3_task-17.cpp
+++ laba-3_task-17.cpp
@@ -2,65 +2,67 @@
 #include "Trie_Tree.h"
 #include <Windows.h>
 #include <string>
-// Функция для проверки чередования гласных и согласных
 
-const std::string gl = "eyuioa";
 
-class my_str {
-public:
-    std::string str;
-    int size;
-};
-my_str operator+(my_str lhs, const char c) {
-    lhs.str += c;
-    ++lhs.size;
-    return lhs;
-}
-int cnt_alternating_words(ttree::ptrNODE t, my_str word, const my_str& substring) {
-    std::cout << word.str << " ";
-    if (word.size >= substring.size) {
-        bool fg = true;
-        int i = 0;
-        while (i < substring.size && fg) {
-            if (word.str[word.size - substring.size + i] != substring.str[i])
-                fg = false;
-            ++i;
-        }
-        if (t->eow || fg)
-            return fg;
-    }
-    else
-        if (t->eow) {
-            return 0;
-        }
+/*Решение
+* В find_substr мы ищем в дереве строки, в которых есть подстрока substring
+* Саму текущую строку мы не держим, т.к. нам не нужно итоговое слово, а только кол-во
+* Мы держим кол-во символов совпадающих с подстрокой, если оно равно длине подстроки, то
+* запускаем рекурсию, которая просто считает все слова, т.к. все слова уже имеют эту подстроку,
+* засунули это в if т.к. мы уже пройдем все слова от слова с подстрокой, следовательно не надо смотреть туда больше
+* if в цикле проверяет будет ли новый символ соответствовать текущему продолжению подстроки
+* если не удовлетворяет, то сбрасываем до нуля, но если новый символ был началом подстроки, то сбрасываем до 1 и след. рекурсия проверит уже на 2й символ
+*/
 
+int cnt_of_words(ttree::ptrNODE t) {
     int res = 0;
+    if (t->eow)
+        res = 1;
     for (int i = 0; i < 26; i++)
         if (t->ptrs[i])
-            res += cnt_alternating_words(t->ptrs[i], word + char(i + 'a'), substring);
+            res += cnt_of_words(t->ptrs[i]);
     return res;
 }
 
-
-std::istream& operator >> (std::istream& in, my_str& my_string)
-{
-    std::string s;
-    in >> s;
-    my_string.str = s;
-    my_string.size = s.length();
-    return in;
+int find_substr(ttree::ptrNODE t, int len_of_cur_word, const std::string& substring) {
+    int res = 0;
+    if (len_of_cur_word == substring.length())
+        res = cnt_of_words(t);
+    else
+        for (int i = 0; i < 26; i++)
+            if (t->ptrs[i]) {
+                if (char(i + 'a') == substring[len_of_cur_word])
+                    res += find_substr(t->ptrs[i], ++len_of_cur_word, substring);
+                else
+                    res += find_substr(t->ptrs[i], char(i + 'a') == substring[0], substring);
+            }
+    return res;
 }
-// Пример использования
+
+std::string words [5] {"app", "apple", "cat", "dog", ""};
+int result[5]{2, 0, 3, 3, 2};
+
 int main() {
     SetConsoleOutputCP(1251);
 
-    ttree::TTREE trie("data.txt");
+    ttree::TTREE trie("test1.txt");
     trie.print(true);
-    my_str word{}, start{};
+   /* std::string word{};
     std::cout << "Задайте необходимое вам слово: ";
-    std::cin >> word;
-    std::cout << "Кол-во слов содержащих подстроку:" << word.str << "\n";
-    std::cout << cnt_alternating_words(trie.get_root(), start , word);
+    std::cin >> word;*/
+
+    
+
+
+    std::cout << "Кол-во слов содержащих подстроку:" << words[4] << " =";
+    std::cout << find_substr(trie.get_root(), 0, words[4]);
 
     return 0;
 }
+
+// функция которая, как только нашла совпадение подстроки запускается для уменьшения потребления ресурсов.
+
+
+
+// собираем слово если дошли до eow то возвращаем 0
+// собираем подстроку
